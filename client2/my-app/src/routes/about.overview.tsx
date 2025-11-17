63c3dfdadcfc261c3ee48f8eb02f2b873d7fd1a4
Sergey Ramonov <serg81789@gmail.com>
2025-11-17 08:32:54 +0300

    lademo png

--- client2/my-app/src/routes/about.overview.tsx
+++ client2/my-app/src/routes/about.overview.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Text, useColorModeValue } from '@chakra-ui/react'
+import { Link } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/about/overview')({
   component: RouteComponent,
@@ -12,6 +13,7 @@ function RouteComponent() {
     <Text color={color}>Управление командой с помощью SSO и RBAC.</Text>
     <Text color={color}>Журналирование активности для мониторинга использования сервисов.</Text>
     <Text color={color}>Внедрение ошибок, хаоса и других форм поведения «на черный день».</Text>
+    <img src='/design_chaos_1.png' alt='logo' width={400} height={400} />
     <Text color={color}>Создание синтетических тестовых данных или импорт данных из CSV-файла или базы данных.</Text>
   </div>
 }
