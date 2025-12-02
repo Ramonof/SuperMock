408177f31df688420a7168bbc1ef20f9d43c79b0
Sergey Ramonov <serg81789@gmail.com>
2025-12-02 14:32:35 +0300

    lademo png 2

--- client2/my-app/src/routes/index.tsx
+++ client2/my-app/src/routes/index.tsx
@@ -58,6 +58,58 @@ function Index() {
               </Box>
             </Container>
           </HStack>
+          <Container maxWidth='120ch'>
+            <HStack>
+              <Container maxWidth='-webkit-max-content'>
+                <Heading size='xl'>
+                  Точный
+                </Heading>
+                <Text>
+                  Моделируемые API, отражающие реальность
+                </Text>
+                <Text>
+                  - Соответствие запросов, ошибки, задержка ответа, динамические ответы
+                </Text>
+                <Text>
+                  - Динамические состояния во время сеансов тестирования
+                </Text>
+                <Text>
+                  - Динамические состояния сеансов тестирования и мощные сценарии
+                </Text>
+              </Container>
+              <Container maxWidth='-webkit-max-content'>
+                <Box boxShadow={'0 0 40px #0550ff40'}>
+                  <img src='/main_page.png' alt='logo' width={'100%'} height={'100%'}/>
+                </Box>
+              </Container>
+            </HStack>
+          </Container>
+          <Container maxWidth='120ch'>
+            <HStack margin={'auto'}>
+              <Container maxWidth='-webkit-max-content'>
+                <Box boxShadow={'0 0 40px #0550ff40'}>
+                  <img src='/spec_gen.png' alt='logo' width={'100%'} height={'100%'}/>
+                </Box>
+              </Container>
+              <Container maxWidth='-webkit-max-content'>
+                <Heading size='xl'>
+                  Гибкий
+                </Heading>
+                <Text>
+                  Получите максимум от OpenAPI
+                </Text>
+                <Text>
+                  - Храните файлы OpenAPI в Git, отправляйте и извлекайте изменения в свои макеты по мере необходимости
+                </Text>
+                <Text>
+                  - Создание прототипов с использованием макетов для быстрого проектирования новых API
+                </Text>
+                <Text>
+                  - Автоматическая генерация спецификаций OpenAPI
+                </Text>
+              </Container>
+            </HStack>
+          </Container>
         </Stack>
   )
 }
